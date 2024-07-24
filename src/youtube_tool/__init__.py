346f8a2078dded0ee31782795fe6d1c91ae72446
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-24 17:25:08 +0545

    feat: Add dependencies and implement YoutubeTool class and tests

--- src/youtube_tool/__init__.py
+++ src/youtube_tool/__init__.py
@@ -1,2 +1,20 @@
-def hello() -> str:
-    return "Hello from youtube-tool!"
+from requests import Session
+from yt_dlp import YoutubeDL
+from yt_dlp.extractor.youtube import YoutubeBaseInfoExtractor
+
+class YoutubeTool:
+    def __init__(self, ydl: YoutubeDL):
+        self.ydl = ydl
+        self.extractor = YoutubeBaseInfoExtractor(ydl)
+
+    def api(self) -> Session:
+        session = Session()
+        session.cookies = self.ydl.cookiejar
+        return session
+
+    def fetch_data(self, url: str) -> dict:
+        return self.ydl.extract_info(url, download=False)
+
+    def get_playlist(self, playlist_id: str):
+        url = f"https://www.youtube.com/watch?v={playlist_id}"
+        return self.fetch_data(url)
---
+++ src/youtube_tool/test_youtube_tool.py
@@ -0,0 +1,7 @@
+def test_number_one():
+    """Docstring"""
+    assert 1 == 1
+
+
+def test_youtube_tool_init_browser():
+    tool = YouTubeTool()
