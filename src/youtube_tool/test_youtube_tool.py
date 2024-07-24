093b9656b951610e97bf992cfbaa84209d660873
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-24 18:09:41 +0545

    refactor(youtube_tool): Update browser variable to use enum type, improve authentication checks

--- src/youtube_tool/test_youtube_tool.py
+++ src/youtube_tool/test_youtube_tool.py
@@ -1,7 +1,11 @@
-from pywin.framework.toolmenu import tools
-from yt_dlp import YoutubeDL
+import pytest
 
-BROWSER = "brave"
+from yt_dlp import YoutubeDL
+from yt_dlp.extractor.youtube import YoutubeBaseInfoExtractor
+
+from src.youtube_tool import CookiesBrowsers
+
+BROWSER: CookiesBrowsers = "vivaldi"
 
 
 def test_number_one():
@@ -12,17 +16,33 @@ def test_number_one():
 def test_youtube_tool_init_browser():
     from . import YoutubeTool
 
-    tool = YoutubeTool("brave")
+    tool = YoutubeTool(BROWSER)
 
-    assert tool.extractor._cookies_passed == True
+    # assert tool.extractor._cookies_passed == True
+    assert tool.extractor.is_authenticated == True
+
+
+def test_youtube_dl_cookiesfrombrowser():
+    browser = "vivaldi"
+    # browser = "brave"
+    ydl = YoutubeDL(
+        {
+            "cookiesfrombrowser": (browser,),
+        }
+    )
+    extractor = YoutubeBaseInfoExtractor(ydl)
+    # print(extractor.cookiejar)
+    assert extractor.is_authenticated == True
 
 
 def test_youtube_tool_fetch_info():
     from . import YoutubeTool
     import json
 
-    tool = YoutubeTool("brave")
-    info = tool.fetch_info("https://www.youtube.com/watch?v=amXz5QsRG-4")
+    tool = YoutubeTool(BROWSER)
+    info = tool.fetch_info(
+        "https://www.youtube.com/watch?v=&list=PLQQsYaXd5huAqbx5q1cZvHqYRhV3Gv56s"
+    )
 
     with open("info.json", "w", encoding="utf-8") as f:
         json.dump(info, f, indent=4)
