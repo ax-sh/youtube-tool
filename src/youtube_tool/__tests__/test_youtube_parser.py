3c0a66c8c10a2eb5721f553d7bbe764bcffa777f
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-24 19:04:37 +0545

    test: Add video info validation test using Pydantic model

--- src/youtube_tool/__tests__/test_youtube_parser.py
+++ src/youtube_tool/__tests__/test_youtube_parser.py
@@ -1,4 +1,8 @@
+import pytest
+
 from typing import TypedDict, List
+
+from pydantic import ValidationError
 
 from ..parser import JsonPath
 from ..types import VideoInfo
@@ -25,3 +29,23 @@ def test_parse_json():
     for i in entries:
         entry = VideoInfo(**i)
         print(entry.uploader)
+
+def test_video_info_validation():
+    # Example dictionary
+    data = {
+        "description": None,
+        "uploader": "Supabase",
+        "uploader_id": "@Supabase",
+        "uploader_url": "https://www.youtube.com/@Supabase",
+        "release_timestamp": None,
+        "availability": None,
+        "live_status": None,
+        "channel_is_verified": None,
+        "__x_forwarded_for_ip": None,
+    }
+
+    # Validate data using the Pydantic model
+    try:
+        video_info = VideoInfo(**data)
+    except ValidationError as e:
+        pytest.fail(f"Validation error: {e}")
