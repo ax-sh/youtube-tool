f53133f259545336da3880c6009179ff0ba275cb
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-24 22:28:24 +0545

    style: Remove commented-out code from YoutubeVideo class

--- src/youtube_tool/types.py
+++ src/youtube_tool/types.py
@@ -25,6 +25,18 @@ class YoutubeVideo(BaseModel):
 
     thumb: str = ""
 
+    # channel_id: str = ""
+    # channel_url: str = ""
+    # channel: str = ""
+    # uploader: str = ""
+    # uploader_id: str = ""
+    # uploader_url: str = ""
+    # release_timestamp: str = ""
+    # availability: str = ""
+    # live_status: str = ""
+    # channel_is_verified: str = ""
+    # __x_forwarded_for_ip: str = ""
+
     class Config:
         extra = "allow"
 
