d9c9642f5c960376d42456f33b638f882847c21c
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-24 23:05:04 +0545

    feat: Add constants.py

---
+++ src/youtube_tool/constants.py
@@ -0,0 +1,9 @@
+from typing_extensions import Final
+
+NETRC_MACHINE: Final[str] = "youtube"
+USER_AGENT: Final[str] = (
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36,gzip(gfe)"
+)
+
+WATCH_LATER_URL: Final[str] = "https://www.youtube.com/playlist?list=WL"
