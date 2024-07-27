d61852e4ce477bec7488683f6890eac0af3a3875
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-28 04:25:48 +0545

    feat: Add Playlist class with remove_video method

--- src/youtube_tool/__init__.py
+++ src/youtube_tool/__init__.py
@@ -13,6 +13,32 @@ class YtcfgDict(TypedDict, total=False):
     VISITOR_DATA: str
 
 
+class Playlist:
+    def __init__(self, playlist_id: str, api_session: YoutubeBaseInfoExtractor):
+        self.playlist_id = playlist_id  # "WL"
+        self.api_session = api_session
+
+    def remove_video(self, video_id: str):
+        action = {
+            "removedVideoId": video_id,
+            "action": "ACTION_REMOVE_VIDEO_BY_VIDEO_ID",
+        }
+        endpoint = "browse/edit_playlist?prettyPrint=false"
+        query = {
+            "actions": [action],
+            "playlistId": self.playlist_id,
+            "params": "CAFAAQ%3D%3D",
+        }
+        response = self.api_session._extract_response(
+            ep=endpoint,
+            query=query,
+            item_id=video_id,
+            note=f"Removing video from {self.playlist_id} playlist",
+            # video_id=video_id,
+        )
+        return response
+
+
 class YoutubeTool:
     OPTIONS = {
         "extract_flat": "in_playlist",
