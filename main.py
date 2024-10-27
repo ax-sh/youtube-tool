7610305ea011758ed0f9c631e8676c096841900a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-10-28 03:23:11 +0545

    feat: add fetch_watchlater function to save watchlist

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from youtube_tool import YoutubeTool
+from pathlib import Path
+import json
 
-def main():
+
+def fetch_watchlater():
     yt = YoutubeTool("vivaldi")
     # yt.remove_video_from_playlist('')
     # url = "https://www.youtube.com/playlist?list=WL"
     wl = yt.fetch_videos_from_watchlist()
-    print(wl)
 
-main()
+    return wl
+
+
+wl = fetch_watchlater()
+path = Path(__file__).parent.absolute()
+data = path / "watchlist.json"
+with data.open("w", encoding="utf-8") as f:
+    json.dump(wl, f, indent=4)
